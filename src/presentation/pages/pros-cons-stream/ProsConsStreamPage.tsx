da2480baa8bffa8328dbdf02d406fe2c5af7f8dd
cafalco96 <carlossfalco96@gmail.com>
2026-03-29 02:58:02 -0500

    add stream pro cons logic

--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,7 +1,89 @@
-import React from 'react'
-
-export const ProsConsStreamPage = () => {
-  return (
-    <div>ProsConsStreamPage</div>
-  )
+import React, { useRef } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
+import { useState } from "react";
+import { ProsConsStreamGeneratorUseCase, ProsConsStreamUseCase } from "../../../core/use-cases";
+interface Message {
+  text: string;
+  isGpt: boolean;
 }
+export const ProsConsStreamPage = () => {
+  const abortController = useRef(new AbortController());
+  const isRunning = useRef(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const handlePost = async (text: string) => {
+    if ( isRunning.current ) {
+      abortController.current.abort();
+      abortController.current = new AbortController();
+    }
+    setIsLoading(true);
+    isRunning.current = true;
+    setMessages((prev) => [...prev, { text, isGpt: false }]);
+    //TODO UseCase
+    // const reader = await ProsConsStreamUseCase(text);
+    // setIsLoading(false);
+    // if (!reader) return;
+    // const decoder = new TextDecoder();
+    // let message = "";
+    // setMessages((messages) => [...messages, { text: message, isGpt: true }]);
+    // while (true) {
+    //   const { done, value } = await reader.read();
+    //   if (done) break;
+    //   const decodedChunk = decoder.decode(value, { stream: true });
+    //   message += decodedChunk;
+    //   setMessages((messages) => {
+    //     const newMessage = [...messages];
+    //     newMessage[newMessage.length - 1].text = message;
+    //     return newMessage;
+    //   });
+    // }
+
+    //Generar ultimo mensaje
+    const stream = ProsConsStreamGeneratorUseCase(text, abortController.current.signal);
+    setIsLoading(false);
+    
+    setMessages((messages) => [...messages, { text: '', isGpt: true }]);
+    for await (const text of stream!) {
+      setMessages((messages) => {
+        const newMessage = [...messages];
+        newMessage[newMessage.length - 1].text = text;
+        return newMessage;
+      });
+    }
+    isRunning.current = false;
+  };
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Bienvenida */}
+          <GptMessage text="Qué deseas comparar hoy?" />
+          {/* Ejemplo de mensaje del usuario */}
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text} />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            ),
+          )}
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe lo que deseas corregir..."
+        disabledCorrections
+      />
+    </div>
+  );
+};
