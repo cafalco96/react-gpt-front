e87389a29b4c6d8ee9a637add1189b5e1c2e56b6
cafalco96 <carlossfalco96@gmail.com>
2026-03-29 02:56:25 -0500

    export new use cases

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -1,1 +1,5 @@
 export * from "./orthography.use-case";
+export * from "./prosCons.use-case";
+export * from "./prosConsStream.use-case";
+export * from "./prosConsStreamGenerator.use-case";
+export * from "./translate.use-case";
