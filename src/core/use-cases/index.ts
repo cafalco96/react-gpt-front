050115a319814503e1493945772239e89ed983ff
cafalco96 <carlossfalco96@gmail.com>
2026-03-29 23:10:58 -0500

    update exports files - uses

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -3,3 +3,4 @@ export * from "./prosCons.use-case";
 export * from "./prosConsStream.use-case";
 export * from "./prosConsStreamGenerator.use-case";
 export * from "./translate.use-case";
+export * from "./textToAudio.use-case";
